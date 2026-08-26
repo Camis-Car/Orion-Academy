a279f18efe858a998b73e1fb9009d615b4117b14
Camila <camilaoc14@gmail.com>
2026-08-25 22:24:15 -0300

    inclui instituições com oferta interestadual SUL

---
+++ instituicoes-com-ofertas-interestaduais-sisu-2026.js
@@ -0,0 +1,8 @@
+window.INSTITUICOES_COM_OFERTAS_INTERESTADUAIS_SISU_2026 = [
+  {"id":"3984","nome":"Fundação Universidade Federal Do Vale Do São Francisco","sigla":"UNIVASF","uf":"BA","sedeUf":"PE","tipo":"Universidade","administracao":"Federal","cidades":["Juazeiro","Senhor Do Bonfim","Paulo Afonso"]},
+  {"id":"15497","nome":"Universidade Da Integração Internacional Da Lusofonia Afro-Brasileira","sigla":"UNILAB","uf":"BA","sedeUf":"CE","tipo":"Universidade","administracao":"Federal","cidades":["São Francisco Do Conde"]},
+  {"id":"3984","nome":"Fundação Universidade Federal Do Vale Do São Francisco","sigla":"UNIVASF","uf":"PI","sedeUf":"PE","tipo":"Universidade","administracao":"Federal","cidades":["São Raimundo Nonato"]},
+  {"id":"15121","nome":"Universidade Federal Da Fronteira Sul","sigla":"UFFS","uf":"PR","sedeUf":"SC","tipo":"Universidade","administracao":"Federal","cidades":["Realeza","Laranjeiras Do Sul"]},
+  {"id":"15121","nome":"Universidade Federal Da Fronteira Sul","sigla":"UFFS","uf":"RS","sedeUf":"SC","tipo":"Universidade","administracao":"Federal","cidades":["Erechim","Cerro Largo","Passo Fundo"]},
+  {"id":"598","nome":"Universidade Federal De Itajubá - Unifei","sigla":"UNIFEI","uf":"SP","sedeUf":"MG","tipo":"Universidade","administracao":"Federal","cidades":["Jaú","Serrana"]}
+];
